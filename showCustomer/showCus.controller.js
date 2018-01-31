682cf4f1e02e24163e6dbae803212deef5e2f898
apotoxin <ais_arm@hotmail.com>
2018-01-31 21:12:09 +0700

    change nav bar and add customer page

---
+++ showCustomer/showCus.controller.js
@@ -0,0 +1,196 @@
+var app = angular.module("ShowCusApp", ['chart.js', 'ui.bootstrap']);
+app.controller("ShowCusCtrl", ['$scope', '$http', function ($scope, $http) {
+    $scope.customers = "";
+    $http.get("http://localhost:3000/customers")
+        .then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(response);
+            $scope.customers = response.data;
+            $scope.AgeChart();
+            //console.log($scope.transactions.length);
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+         //select data 
+    $scope.selectedCustomer = $scope.customers[0];
+    $scope.selectCustomer = function (index) {
+        $scope.selectedCustomer = $scope.customers[index];
+        $scope.successMessage = undefined;
+        $scope.errorMessage = undefined;
+        $scope.addMessage = undefined;
+    }
+
+    $scope.AgeChart = function () {
+        $scope.nameDoughnutChart = " Customer AgeChart";
+        $scope.ids_data = [];
+        $scope.num = [];
+        var data = $scope.customers;
+
+        for (var i = 0; i < $scope.customers.length; i++) {
+            if ($scope.ids_data.indexOf(data[i].age) == -1) {
+                $scope.ids_data.push(data[i].age);
+            }
+        }
+        for (var p = 0; p < $scope.ids_data.length; p++) {
+            $scope.num[p] = 0;
+        }
+
+        for (var j = 0; j < $scope.customers.length; j++) {
+            for (var k = 0; k < $scope.ids_data.length; k++) {
+                if (data[j].age == $scope.ids_data[k]) {
+                    $scope.num[k] += 1;
+                }
+            }
+
+        }
+
+        for (var l = 0; l < $scope.ids_data.length; l++) {
+            $scope.ids_data[l] = "Age " + $scope.ids_data[l];
+        }
+        console.log($scope.ids_data);
+        console.log($scope.num);
+    }
+
+    $scope.SexChart = function () {
+        $scope.nameDoughnutChart = " Customer SexChart";
+        $scope.ids_data = [];
+        $scope.num = [];
+        var data = $scope.customers;
+
+        for (var i = 0; i < $scope.customers.length; i++) {
+            if ($scope.ids_data.indexOf(data[i].sex) == -1) {
+                $scope.ids_data.push(data[i].sex);
+            }
+        }
+        for (var p = 0; p < $scope.ids_data.length; p++) {
+            $scope.num[p] = 0;
+        }
+
+        for (var j = 0; j < $scope.customers.length; j++) {
+            for (var k = 0; k < $scope.ids_data.length; k++) {
+                if (data[j].sex == $scope.ids_data[k]) {
+                    $scope.num[k] += 1;
+                }
+            }
+
+        }
+
+        for (var l = 0; l < $scope.ids_data.length; l++) {
+            $scope.ids_data[l] = "Sex " + $scope.ids_data[l];
+        }
+        console.log($scope.ids_data);
+        console.log($scope.num);
+    }
+
+    $scope.JobChart = function () {
+        $scope.nameDoughnutChart = " Customer JobChart";
+        $scope.ids_data = [];
+        $scope.num = [];
+        var data = $scope.customers;
+
+        for (var i = 0; i < $scope.customers.length; i++) {
+            if ($scope.ids_data.indexOf(data[i].job) == -1) {
+                $scope.ids_data.push(data[i].job);
+            }
+        }
+        for (var p = 0; p < $scope.ids_data.length; p++) {
+            $scope.num[p] = 0;
+        }
+
+        for (var j = 0; j < $scope.customers.length; j++) {
+            for (var k = 0; k < $scope.ids_data.length; k++) {
+                if (data[j].job == $scope.ids_data[k]) {
+                    $scope.num[k] += 1;
+                }
+            }
+
+        }
+
+        for (var l = 0; l < $scope.ids_data.length; l++) {
+            $scope.ids_data[l] = "Job " + $scope.ids_data[l];
+        }
+        console.log($scope.ids_data);
+        console.log($scope.num);
+    }
+
+    $scope.SalaryChart = function () {
+        $scope.nameDoughnutChart = " Customer SalaryChart";
+        $scope.ids_data = [];
+        $scope.num = [];
+        var data = $scope.customers;
+
+        for (var i = 0; i < $scope.customers.length; i++) {
+            if ($scope.ids_data.indexOf(data[i].salary) == -1) {
+                $scope.ids_data.push(data[i].salary);
+            }
+        }
+        for (var p = 0; p < $scope.ids_data.length; p++) {
+            $scope.num[p] = 0;
+        }
+
+        for (var j = 0; j < $scope.customers.length; j++) {
+            for (var k = 0; k < $scope.ids_data.length; k++) {
+                if (data[j].salary == $scope.ids_data[k]) {
+                    $scope.num[k] += 1;
+                }
+            }
+
+        }
+
+        for (var l = 0; l < $scope.ids_data.length; l++) {
+            $scope.ids_data[l] = "Salary " + $scope.ids_data[l];
+        }
+        console.log($scope.ids_data);
+        console.log($scope.num);
+    }
+    $scope.ShowroomChart = function () {
+        $scope.nameDoughnutChart = " Customer ShowroomChart";
+        $scope.ids_data = [];
+        $scope.num = [];
+        var data = $scope.customers;
+
+        for (var i = 0; i < $scope.customers.length; i++) {
+            if ($scope.ids_data.indexOf(data[i].showroom_id) == -1) {
+                $scope.ids_data.push(data[i].showroom_id);
+            }
+        }
+        for (var p = 0; p < $scope.ids_data.length; p++) {
+            $scope.num[p] = 0;
+        }
+
+        for (var j = 0; j < $scope.customers.length; j++) {
+            for (var k = 0; k < $scope.ids_data.length; k++) {
+                if (data[j].showroom_id == $scope.ids_data[k]) {
+                    $scope.num[k] += 1;
+                }
+            }
+
+        }
+
+        for (var l = 0; l < $scope.ids_data.length; l++) {
+            $scope.ids_data[l] = "Showroom_Id " + $scope.ids_data[l];
+        }
+        console.log($scope.ids_data);
+        console.log($scope.num);
+    }
+
+}]);
+
+app.controller('TabsDemoCtrl', function ($scope, $window) {
+    $scope.tabs = [
+      { title:'Dynamic Title 1', content:'Dynamic content 1' },
+      { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+    ];
+  
+    $scope.alertMe = function() {
+      setTimeout(function() {
+        $window.alert('You\'ve selected the alert tab!');
+      });
+    };
+  
+    $scope.model = {
+      name: 'Tabs'
+    };
+  });
--- showTransaction/showTran.controller.js
+++ showTransaction/showTran.controller.js
@@ -8,75 +8,135 @@ app.controller("ShowTranCtrl", ['$scope', '$http', function ($scope, $http) {
             // when the response is available
             console.log(response);
             $scope.transactions = response.data;
-            $scope.MusicBoxChart();
+            $scope.ShowroomChart();
+            $scope.BarChart(0);
             //console.log($scope.transactions.length);
         }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         });
-    $scope.selectedTransaction = $scope.transactions[0];
-    $scope.selectTransaction = function (index) {
-        $scope.selectedTransaction = $scope.transactions[index];
-        $scope.successMessage = undefined;
-        $scope.errorMessage = undefined;
-        $scope.addMessage = undefined;
-    }
- 
-    $scope.ids_data = [];
-    $scope.num = [];
-    $scope.MusicBoxChart = function(){
+    //select data 
+    // $scope.selectedTransaction = $scope.transactions[0];
+    // $scope.selectTransaction = function (index) {
+    //     $scope.selectedTransaction = $scope.transactions[index];
+    //     $scope.successMessage = undefined;
+    //     $scope.errorMessage = undefined;
+    //     $scope.addMessage = undefined;
+    // }
+
+    //chart of musicbox
+    $scope.MusicBoxChart = function () {
+        $scope.nameDoughnutChart = "MusicBoxChart";
+        $scope.ids_data = [];
+        $scope.num = [];
         var data = $scope.transactions;
 
-        for(var i =0;i<$scope.transactions.length;i++){
-            if ( $scope.ids_data.indexOf(data[i].music_box_id) == -1) {
+        for (var i = 0; i < $scope.transactions.length; i++) {
+            if ($scope.ids_data.indexOf(data[i].music_box_id) == -1) {
                 $scope.ids_data.push(data[i].music_box_id);
             }
         }
-        for(var p = 0 ; p<$scope.ids_data.length;p++){
-            $scope.num[p]=0;
+        for (var p = 0; p < $scope.ids_data.length; p++) {
+            $scope.num[p] = 0;
         }
 
-        for(var j =0; j <$scope.transactions.length;j++){
-            for(var k =0; k <$scope.ids_data.length;k++){
-                if(data[j].music_box_id==$scope.ids_data[k]){
-                    $scope.num[k]+=1;
+        for (var j = 0; j < $scope.transactions.length; j++) {
+            for (var k = 0; k < $scope.ids_data.length; k++) {
+                if (data[j].music_box_id == $scope.ids_data[k]) {
+                    $scope.num[k] += 1;
                 }
             }
+
+        }
+
+        for (var l = 0; l < $scope.ids_data.length; l++) {
+            $scope.ids_data[l] = "MusicBox_id " + $scope.ids_data[l];
+        }
+        console.log($scope.ids_data);
+        console.log($scope.num);
+    }
+
+    //chart of showroom
+    $scope.ShowroomChart = function () {
+        $scope.nameDoughnutChart = "ShowroomChart";
+        $scope.ids_data = [];
+        $scope.num = [];
+        var data = $scope.transactions;
+        for (var i = 0; i < $scope.transactions.length; i++) {
+            if ($scope.ids_data.indexOf(data[i].showroom_id) == -1) {
+                $scope.ids_data.push(data[i].showroom_id);
+            }
+        }
+        for (var p = 0; p < $scope.ids_data.length; p++) {
+            $scope.num[p] = 0;
+        }
+
+        for (var j = 0; j < $scope.transactions.length; j++) {
+            for (var k = 0; k < $scope.ids_data.length; k++) {
+                if (data[j].showroom_id == $scope.ids_data[k]) {
+                    $scope.num[k] += 1;
+                }
+            }
+
+        }
+
+        for (var l = 0; l < $scope.ids_data.length; l++) {
+            $scope.ids_data[l] = "Showroom_id " + $scope.ids_data[l];
+        }
+        console.log($scope.ids_data);
+        console.log($scope.num);
+    }
+
+    //dropdown
+    $scope.DropdownItems = ["Showroom", "Music Box"];
+    $scope.Change = function (DropdownItems) {
+        if (DropdownItems == "Showroom") {
+            console.log("click Showroom");
+            $scope.ShowroomChart();
             
         }
-        
-        for(var l = 0 ; l<$scope.ids_data.length;l++){
-            $scope.ids_data[l] = "id"+$scope.ids_data[l];
+        else {
+            console.log("click Music Box");
+            $scope.MusicBoxChart();
+            
         }
-        console.log($scope.ids_data); 
-        console.log($scope.num); 
+
     }
 
+    //bar chart
+    $scope.BarChart = function(type){
+        var date = new Date();
+        $scope.series = ['MusicBox'];
+
+        if(type==0){
+            $scope.nameBarChart = "Last 7 Day";
+            // last 7 day
+        $scope.x_axis = ['7', '8', '9', '10', '11', '12', '13'];
+        $scope.y_axis = [[65, 59, 80, 81, 56, 55, 90]];
+        }
+        else if(type==1) {
+            $scope.nameBarChart = "This Month";
+            //this month
+            var month  = date.getMonth();
+            $scope.x_axis = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15',
+                            '16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'];
+            $scope.y_axis =  [[65, 59, 80, 81, 56, 55, 90, 56, 55, 90,
+                65, 59, 80, 81, 56, 55, 90, 56, 55, 90,
+                65, 59, 80, 81, 56, 55, 90, 56, 55, 90]];
+            
+        }
+        else  {
+            $scope.nameBarChart = "This Year";
+            //this year
+            var year  = date.getFullYear();
+            $scope.x_axis = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'];
+            $scope.y_axis = [[65, 59, 80, 81, 56, 55, 90, 56, 55, 90,
+                65, 59]];
+        }
+    }
     
 
-   // $scope.aaaa();
-
-
 }]);
-
-app.controller("DoughnutCtrl", function ($scope) {
-    $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-    $scope.data = [300, 500, 100];
-});
-
-app.controller("DropDownCtrl", function($scope) {
-
-    $scope.items= ["Showroom","Music Box"];
-    $scope.Change = function(item){
-        if(item=="Showroom"){
-            console.log("click Showroom");
-        }
-        else{
-            console.log("click Music Box");
-        }
-
-    }
-});
 
 
 
