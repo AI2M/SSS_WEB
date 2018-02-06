78af2a1385ce5ef800a9a29cb9e264c9141f72a3
apotoxin <ais_arm@hotmail.com>
2018-02-06 21:55:39 +0700

    add customer api

--- js/showCus.controller.js
+++ js/showCus.controller.js
@@ -1,18 +1,66 @@
 var app = angular.module("SSS", ['chart.js', 'ui.bootstrap']);
 app.controller("ShowCusCtrl", ['$scope', '$http', function ($scope, $http) {
     $scope.customers = "";
-    $http.get("http://localhost:3000/customers")
-        .then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            console.log(response);
-            $scope.customers = response.data;
-            $scope.AgeChart();
-            //console.log($scope.transactions.length);
-        }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
+    $scope.loadData = function(type){
+        if(type==1){
+            $http.get("http://localhost/SSS_web_api/getCustomerData.php/?type=age")
+            .then(function successCallback(response) {
+                console.log(response);
+                $scope.customers = response.data.customers;
+                $scope.AgeChart();
+
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+        else if(type==2){
+            $http.get("http://localhost/SSS_web_api/getCustomerData.php/?type=sex")
+            .then(function successCallback(response) {
+                console.log(response);
+                $scope.customers = response.data.customers;
+                $scope.SexChart();
+
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+        else if(type==3){
+            $http.get("http://localhost/SSS_web_api/getCustomerData.php/?type=job")
+            .then(function successCallback(response) {
+                console.log(response);
+                $scope.customers = response.data.customers;
+                $scope.JobChart();
+
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+        else if(type==4){
+            $http.get("http://localhost/SSS_web_api/getCustomerData.php/?type=salary")
+            .then(function successCallback(response) {
+                console.log(response);
+                $scope.customers = response.data.customers;
+                $scope.SalaryChart();
+
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+        else if(type==5){
+            $http.get("http://localhost/SSS_web_api/getCustomerData.php/?type=showroom_id")
+            .then(function successCallback(response) {
+                console.log(response);
+                $scope.customers = response.data.customers;
+                $scope.ShowroomChart();
+
+            }, function errorCallback(response) {
+                console.log(response);
+            });
+        }
+    }
+
+    $scope.loadData(1);
+    
          //select data 
     $scope.selectedCustomer = $scope.customers[0];
     $scope.selectCustomer = function (index) {
@@ -28,27 +76,14 @@ app.controller("ShowCusCtrl", ['$scope', '$http', function ($scope, $http) {
         $scope.num = [];
         var data = $scope.customers;
 
-        for (var i = 0; i < $scope.customers.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             if ($scope.ids_data.indexOf(data[i].age) == -1) {
-                $scope.ids_data.push(data[i].age);
+                $scope.ids_data.push("Age " + data[i].age);
             }
         }
-        for (var p = 0; p < $scope.ids_data.length; p++) {
-            $scope.num[p] = 0;
-        }
-
-        for (var j = 0; j < $scope.customers.length; j++) {
-            for (var k = 0; k < $scope.ids_data.length; k++) {
-                if (data[j].age == $scope.ids_data[k]) {
-                    $scope.num[k] += 1;
-                }
-            }
-
-        }
-
-        for (var l = 0; l < $scope.ids_data.length; l++) {
-            $scope.ids_data[l] = "Age " + $scope.ids_data[l];
-        }
+        for (var j = 0; j < data.length; j++) {
+            $scope.num.push(data[j].age_count);
+         }
         console.log($scope.ids_data);
         console.log($scope.num);
     }
@@ -59,27 +94,15 @@ app.controller("ShowCusCtrl", ['$scope', '$http', function ($scope, $http) {
         $scope.num = [];
         var data = $scope.customers;
 
-        for (var i = 0; i < $scope.customers.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             if ($scope.ids_data.indexOf(data[i].sex) == -1) {
-                $scope.ids_data.push(data[i].sex);
+                $scope.ids_data.push("sex " + data[i].sex);
             }
         }
-        for (var p = 0; p < $scope.ids_data.length; p++) {
-            $scope.num[p] = 0;
-        }
+        for (var j = 0; j < data.length; j++) {
+            $scope.num.push(data[j].sex_count);
+         }
 
-        for (var j = 0; j < $scope.customers.length; j++) {
-            for (var k = 0; k < $scope.ids_data.length; k++) {
-                if (data[j].sex == $scope.ids_data[k]) {
-                    $scope.num[k] += 1;
-                }
-            }
-
-        }
-
-        for (var l = 0; l < $scope.ids_data.length; l++) {
-            $scope.ids_data[l] = "Sex " + $scope.ids_data[l];
-        }
         console.log($scope.ids_data);
         console.log($scope.num);
     }
@@ -90,27 +113,14 @@ app.controller("ShowCusCtrl", ['$scope', '$http', function ($scope, $http) {
         $scope.num = [];
         var data = $scope.customers;
 
-        for (var i = 0; i < $scope.customers.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             if ($scope.ids_data.indexOf(data[i].job) == -1) {
-                $scope.ids_data.push(data[i].job);
+                $scope.ids_data.push("job " + data[i].job);
             }
         }
-        for (var p = 0; p < $scope.ids_data.length; p++) {
-            $scope.num[p] = 0;
-        }
-
-        for (var j = 0; j < $scope.customers.length; j++) {
-            for (var k = 0; k < $scope.ids_data.length; k++) {
-                if (data[j].job == $scope.ids_data[k]) {
-                    $scope.num[k] += 1;
-                }
-            }
-
-        }
-
-        for (var l = 0; l < $scope.ids_data.length; l++) {
-            $scope.ids_data[l] = "Job " + $scope.ids_data[l];
-        }
+        for (var j = 0; j < data.length; j++) {
+            $scope.num.push(data[j].job_count);
+         }
         console.log($scope.ids_data);
         console.log($scope.num);
     }
@@ -121,27 +131,14 @@ app.controller("ShowCusCtrl", ['$scope', '$http', function ($scope, $http) {
         $scope.num = [];
         var data = $scope.customers;
 
-        for (var i = 0; i < $scope.customers.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             if ($scope.ids_data.indexOf(data[i].salary) == -1) {
-                $scope.ids_data.push(data[i].salary);
+                $scope.ids_data.push("salary " + data[i].salary);
             }
         }
-        for (var p = 0; p < $scope.ids_data.length; p++) {
-            $scope.num[p] = 0;
-        }
-
-        for (var j = 0; j < $scope.customers.length; j++) {
-            for (var k = 0; k < $scope.ids_data.length; k++) {
-                if (data[j].salary == $scope.ids_data[k]) {
-                    $scope.num[k] += 1;
-                }
-            }
-
-        }
-
-        for (var l = 0; l < $scope.ids_data.length; l++) {
-            $scope.ids_data[l] = "Salary " + $scope.ids_data[l];
-        }
+        for (var j = 0; j < data.length; j++) {
+            $scope.num.push(data[j].salary_count);
+         }
         console.log($scope.ids_data);
         console.log($scope.num);
     }
@@ -151,27 +148,14 @@ app.controller("ShowCusCtrl", ['$scope', '$http', function ($scope, $http) {
         $scope.num = [];
         var data = $scope.customers;
 
-        for (var i = 0; i < $scope.customers.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             if ($scope.ids_data.indexOf(data[i].showroom_id) == -1) {
-                $scope.ids_data.push(data[i].showroom_id);
+                $scope.ids_data.push("showroom_id " + data[i].showroom_id);
             }
         }
-        for (var p = 0; p < $scope.ids_data.length; p++) {
-            $scope.num[p] = 0;
-        }
-
-        for (var j = 0; j < $scope.customers.length; j++) {
-            for (var k = 0; k < $scope.ids_data.length; k++) {
-                if (data[j].showroom_id == $scope.ids_data[k]) {
-                    $scope.num[k] += 1;
-                }
-            }
-
-        }
-
-        for (var l = 0; l < $scope.ids_data.length; l++) {
-            $scope.ids_data[l] = "Showroom_Id " + $scope.ids_data[l];
-        }
+        for (var j = 0; j < data.length; j++) {
+            $scope.num.push(data[j].counts);
+         }
         console.log($scope.ids_data);
         console.log($scope.num);
     }
